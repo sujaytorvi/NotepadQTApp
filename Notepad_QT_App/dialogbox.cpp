1627a1a0e134ab6b8294be260a77b44e1c0c4fc1
Sujay Torvi <53830055+sujaytorvi@users.noreply.github.com>
2021-10-21 16:22:42 +0530

    Add files via upload

--- Notepad_QT_App/dialogbox.cpp
+++ Notepad_QT_App/dialogbox.cpp
@@ -1,6 +1,6 @@
 #include "dialogbox.h"
 #include "ui_dialogbox.h"
-
+#include "QScrollArea"
 DialogBox::DialogBox(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::DialogBox)
@@ -10,6 +10,10 @@ DialogBox::DialogBox(QWidget *parent) :
     QPixmap pm(":/ImageResource/0001.jpg");
     ui->label->setPixmap(pm);
     ui->label->setScaledContents(true);
+    QScrollArea *scrollarea = new QScrollArea(this);
+    scrollarea->setBackgroundRole(QPalette :: Dark);
+    scrollarea->setWidget(ui->label);
+
 }
 
 DialogBox::~DialogBox()
