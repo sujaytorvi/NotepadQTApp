dd984cf8c899d51cf4817766ea55c210e78c6c79
Sujay Torvi <53830055+sujaytorvi@users.noreply.github.com>
2021-10-21 16:23:37 +0530

    Add files via upload

--- linux-release/ui_dialogbox.h
+++ linux-release/ui_dialogbox.h
@@ -14,6 +14,8 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QDialog>
 #include <QtWidgets/QLabel>
+#include <QtWidgets/QScrollArea>
+#include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
 
@@ -21,18 +23,28 @@ class Ui_DialogBox
 {
 public:
     QLabel *label;
+    QScrollArea *scrollArea;
+    QWidget *scrollAreaWidgetContents;
 
     void setupUi(QDialog *DialogBox)
     {
         if (DialogBox->objectName().isEmpty())
             DialogBox->setObjectName(QString::fromUtf8("DialogBox"));
-        DialogBox->resize(547, 674);
+        DialogBox->resize(547, 401);
         QIcon icon;
         icon.addFile(QString::fromUtf8(":/ImageResource/resume.png"), QSize(), QIcon::Normal, QIcon::Off);
         DialogBox->setWindowIcon(icon);
         label = new QLabel(DialogBox);
         label->setObjectName(QString::fromUtf8("label"));
-        label->setGeometry(QRect(0, 0, 541, 671));
+        label->setGeometry(QRect(0, 0, 541, 691));
+        scrollArea = new QScrollArea(DialogBox);
+        scrollArea->setObjectName(QString::fromUtf8("scrollArea"));
+        scrollArea->setGeometry(QRect(520, 0, 20, 511));
+        scrollArea->setWidgetResizable(true);
+        scrollAreaWidgetContents = new QWidget();
+        scrollAreaWidgetContents->setObjectName(QString::fromUtf8("scrollAreaWidgetContents"));
+        scrollAreaWidgetContents->setGeometry(QRect(0, 0, 18, 509));
+        scrollArea->setWidget(scrollAreaWidgetContents);
 
         retranslateUi(DialogBox);
 
